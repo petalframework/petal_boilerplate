5702ff82fc55c86c2af9db5e82bf04ee7b19489d
Matthew Platts <plattsmw@gmail.com>
2021-12-07 11:34:06 +1100

    0.6.0

--- mix.exs
+++ mix.exs
@@ -49,7 +49,7 @@ defmodule PetalBoilerplate.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:petal_components, "~> 0.5.1"}
+      {:petal_components, "~> 0.6.0"}
     ]
   end
 

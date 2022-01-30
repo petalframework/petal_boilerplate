d3e93050c2e9cf8f1965704d9fcb68a43447f79d
Nicolas Hoban <nhobes@gmail.com>
2022-01-31 10:44:21 +1100

    [Bug fix] fixed project renaming script.

--- rename_phoenix_project.sh
+++ rename_phoenix_project.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
+export LC_CTYPE=C
+export LANG=C
 
 # How to run:
-# Modify the variables below to your new app name. Then in the terminal run:
-# > sh rename_phoenix_project.sh
+# 1. Ensure you've checked your files into a git repo (`git init .`, `git add -A`, `git commit -m 'first'`)
+# 2. Modify these two values to your new app name
+NEW_NAME="YourAppName"
+NEW_OTP="your_app_name"
+# 3. Execute the script in terminal: `sh rename_phoenix_project.sh`
 
 set -e
 
 if ! command -v ack &> /dev/null
 then
-    echo "\`ack\` could not be found. Please install it before continuing"
+    echo "\`ack\` could not be found. Please install it before continuing (Mac: brew install ack)."
     exit 1
 fi
 
 CURRENT_NAME="PetalBoilerplate"
 CURRENT_OTP="petal_boilerplate"
-
-# TODO: MODIFY THESE TWO VARIABLES BEFORE RUNNING THE SCRIPT
-NEW_NAME="YourAppName"
-NEW_OTP="your_app_name"
 
 ack -l $CURRENT_NAME --ignore-file=is:rename_phoenix_project.sh | xargs sed -i '' -e "s/$CURRENT_NAME/$NEW_NAME/g"
 ack -l $CURRENT_OTP --ignore-file=is:rename_phoenix_project.sh | xargs sed -i '' -e "s/$CURRENT_OTP/$NEW_OTP/g"

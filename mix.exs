31892643d481cf741a63f4a272b6bed381156e35
Nicolas Hoban <nhobes@gmail.com>
2022-03-07 10:57:58 +1100

    bump version number

--- mix.exs
+++ mix.exs
@@ -49,7 +49,7 @@ defmodule PetalBoilerplate.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:petal_components, "~> 0.13.2"},
+      {:petal_components, "~> 0.13.3"},
       {:tailwind, "~> 0.1", runtime: Mix.env() == :dev}
     ]
   end

71f482413df50f2a7c10a23568dd2bd7aab38a7f
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2022-07-15 09:05:41 +0200

    :bomb: playing with path parsing in request

--- lib/eblox_web/controllers/post_controller.ex
+++ lib/eblox_web/controllers/post_controller.ex
@@ -1,13 +1,20 @@
 defmodule EbloxWeb.PostController do
   use EbloxWeb, :controller
 
-  def show(conn, %{"id" => [<<year::binary-size(4), _::binary>> = id]}) do
-    html =
-      case Siblings.payload(Eblox.Siblings, Path.join(year, id)) do
-        %{html: html} -> html
-        other -> "<b font-color='white'>#{inspect(other)}</b>"
-      end
-
+  def show(conn, %{"id" => [<<year::binary-size(4), ?-, _::binary>> = id]}) do
+    html = year |> Path.join(id) |> html()
     render(conn, "show.html", content: html)
+  end
+
+  def show(conn, %{"id" => [id]}) do
+    html = "texts" |> Path.join(id) |> html()
+    render(conn, "show.html", content: html)
+  end
+
+  defp html(path) do
+    case Siblings.payload(Eblox.Siblings, path) do
+      %{html: html} -> html
+      other -> "<b font-color='white'>#{inspect(other)}</b>"
+    end
   end
 end

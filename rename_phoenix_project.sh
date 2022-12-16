27c9bec47b6313c422044167427b780cf4dc0870
Serge Aleynikov <saleyn@gmail.com>
2022-12-16 09:17:40 -0500

    Improve the project renaming script

--- rename_phoenix_project.sh
+++ rename_phoenix_project.sh
@@ -3,32 +3,79 @@ export LC_CTYPE=C
 export LANG=C
 
 # How to run:
-# 1. Ensure you've checked your files into a git repo (`git init .`, `git add -A`, `git commit -m 'first'`)
-# 2. Modify these two values to your new app name
-NEW_NAME="YourAppName"
-NEW_OTP="your_app_name"
-# 3. Execute the script in terminal: `sh rename_phoenix_project.sh`
+# - Execute the script in terminal: `sh rename_phoenix_project.sh -a your_app_name`
+
+SCRIPT=${0##*/}
+
+GIT="git"
+V="-v"
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -a|--app)
+      shift
+      NEW_OTP="${1// /}"
+      if [ -n "$(echo "$NEW_OTP" | sed -n -e '/[A-Z]/p' -e '/^-/p')" ]; then
+        echo "Use snake case for the app name (e.g. 'my_app')" && exit 1
+      fi
+      NEW_NAME="$(echo $NEW_OTP | sed -r 's/(^|_)([a-z])/\U\2/g')"
+      ;;
+    --no-git)
+      GIT="";;
+    -q|--quiet)
+      unset V;;
+    -h|--help)
+      echo "Usage: $SCRIPT [-a|--app] app_name [--no-git] [-q|--quiet] [-h|--help]"
+      echo
+      echo "This script is used to rename files in this boilerplate project."
+      echo "  - If this project is not checked into a git repo, use the '--no-git' option"
+      echo "  - Pass the '-a' option to specify your OTP application name"
+      echo
+      echo "Options:"
+      echo "  -a|--app app_name   - Use this OTP application name"
+      echo "  --no-git            - Use 'mv' command instead of 'git mv'"
+      echo "  -q|--quiet          - Suppress verbosity"
+      echo "  -h|--help           - Print this help screen"
+      exit 1;;
+    *)
+      echo "Invalid option: $1"
+      exit 1;;
+  esac
+  shift
+done
+
+[ -z "$NEW_OTP" -o -z "$NEW_NAME" ] && echo "Missing required '-a' argument!" && exit 1
 
 set -e
-
-if ! command -v ack &> /dev/null
-then
-    echo "\`ack\` could not be found. Please install it before continuing (Mac: brew install ack)."
-    exit 1
-fi
 
 CURRENT_NAME="PetalBoilerplate"
 CURRENT_OTP="petal_boilerplate"
 
-ack -l $CURRENT_NAME --ignore-file=is:rename_phoenix_project.sh | xargs sed -i '' -e "s/$CURRENT_NAME/$NEW_NAME/g"
-ack -l $CURRENT_OTP --ignore-file=is:rename_phoenix_project.sh | xargs sed -i '' -e "s/$CURRENT_OTP/$NEW_OTP/g"
+while true; do
+  echo -en "Rename files containing '$CURRENT_OTP' to '$NEW_OTP' [Y/n]: "
+  read -r yn
+  case "${yn,,}" in
+    n) exit 1;;
+    y) break;;
+  esac
+done
 
-git mv lib/$CURRENT_OTP lib/$NEW_OTP
-git mv lib/$CURRENT_OTP.ex lib/$NEW_OTP.ex
-git mv lib/${CURRENT_OTP}_web lib/${NEW_OTP}_web
-git mv lib/${CURRENT_OTP}_web.ex lib/${NEW_OTP}_web.ex
+function replace() {
+  local WORD=$1
+  local SED=$2
+  local ECHO=$3
 
-# Uncomment this if you have written tests in the folder test/petal_boilerplate
-# git mv test/$CURRENT_OTP test/$NEW_OTP
+  grep -l -r $WORD --exclude=$SCRIPT | xargs sed -i -e "$SED"
+  [ -n $V ] && echo "==> $ECHO"
+}
 
-git mv test/${CURRENT_OTP}_web test/${NEW_OTP}_web
+replace "$CURRENT_NAME" "s/$CURRENT_NAME/$NEW_NAME/g" "Replaced application name"
+replace "$CURRENT_OTP"  "s/$CURRENT_OTP/$NEW_OTP/g"   "Replaced OTP application name"
+
+${GIT} mv $V lib/$CURRENT_OTP lib/$NEW_OTP
+${GIT} mv $V lib/$CURRENT_OTP.ex lib/$NEW_OTP.ex
+${GIT} mv $V lib/${CURRENT_OTP}_web lib/${NEW_OTP}_web
+${GIT} mv $V lib/${CURRENT_OTP}_web.ex lib/${NEW_OTP}_web.ex
+${GIT} mv $V test/${CURRENT_OTP}_web test/${NEW_OTP}_web
+
+[ -d test/$CURRENT_OTP ] && ${GIT} mv $V test/$CURRENT_OTP test/$NEW_OTP

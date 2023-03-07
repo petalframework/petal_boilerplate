df9674aa7ae43c7af66643ad6b55c94218900dce
Matthew Platts <plattsmw@gmail.com>
2023-03-08 10:35:56 +1100

    Update phx to 1.7.1

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule PetalBoilerplate.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.7.0-rc.3", override: true},
+      {:phoenix, "~> 1.7.1"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},

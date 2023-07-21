21a70bec1060872d1ee88a23d31f8d2fcfd4cfc0
Matthew Platts <plattsmw@gmail.com>
2023-07-22 09:37:15 +1000

    Update petal components

--- mix.exs
+++ mix.exs
@@ -51,7 +51,7 @@ defmodule PetalBoilerplate.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:petal_components, "~> 1.2"},
+      {:petal_components, "~> 1.3"},
       {:rename_project, "~> 0.1.0", only: :dev}
     ]
   end

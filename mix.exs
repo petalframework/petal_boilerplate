b334c267819d5f75ad8d240129466a8071dc9dc0
Hamish Murphy <hamishmurphy@gmail.com>
2024-09-21 19:43:29 +1000

    Use latest Petal Components
    in_umbrella is for petel_development

--- mix.exs
+++ mix.exs
@@ -56,7 +56,7 @@ defmodule PetalBoilerplate.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:petal_components, in_umbrella: true},
+      {:petal_components, "~> 2.0.3"},
       {:rename_project, "~> 0.1.0", only: :dev}
     ]
   end

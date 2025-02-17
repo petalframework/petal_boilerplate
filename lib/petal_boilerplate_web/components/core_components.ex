0ec63f1625f5012394514452dd140a96b7065e2e
Hamish Murphy <hamishmurphy@gmail.com>
2025-02-17 17:04:32 +1100

    Corrects default :time

--- lib/petal_boilerplate_web/components/core_components.ex
+++ lib/petal_boilerplate_web/components/core_components.ex
@@ -679,6 +679,7 @@ defmodule PetalBoilerplateWeb.CoreComponents do
   def show(js \\ %JS{}, selector) do
     JS.show(js,
       to: selector,
+      time: 300,
       transition:
         {"transition-all transform ease-out duration-300",
          "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95",
@@ -689,7 +690,6 @@ defmodule PetalBoilerplateWeb.CoreComponents do
   def hide(js \\ %JS{}, selector) do
     JS.hide(js,
       to: selector,
-      time: 200,
       transition:
         {"transition-all transform ease-in duration-200",
          "opacity-100 translate-y-0 sm:scale-100",
